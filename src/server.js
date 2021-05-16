0593c66c28b8b629c7dd702b1060cdf5bc88dbc2
Anton <workglebov@gmail.com>
2021-05-16 22:19:25 +0300

    Return eslint-no-console warning

--- src/server.js
+++ src/server.js
@@ -2,6 +2,5 @@ const { PORT } = require('./common/config');
 const app = require('./app');
 
 app.listen(PORT, () =>
-  // eslint-disable-next-line no-console
   console.log(`App is running on http://localhost:${PORT}`)
 );

9d0ae6528198ae044e58d3a9024c4b26f51392f5
Anton <workglebov@gmail.com>
2021-05-26 10:45:36 +0300

    feat: add Board jsdoc

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -2,30 +2,51 @@ const { deleteBoardInTasks } = require('../tasks/task.memory.repository');
 
 const BOARDS = [];
 
-const deleteBoard = async (boardId) => {
-  // TODO: mock implementation. should be replaced during task development
-  const boardIndex = BOARDS.findIndex((board) => board && board.id === boardId);
-  if (boardIndex >= 0) {
-    BOARDS.splice(boardIndex, 1);
-    await deleteBoardInTasks(boardId);
-    return BOARDS;
-  }
-  return false;
-};
-
+/**
+ * Returns all boards
+ *
+ * @async
+ * @function getAllBoards
+ * @returns {Promise.<Board[]>} array of boards
+ */
 const getAllBoards = async () =>
   // TODO: mock implementation. should be replaced during task development
   BOARDS;
 
-const getBoardById = async (id) =>
-  // TODO: mock implementation. should be replaced during task development
-  BOARDS.find((board) => board && board.id === id);
+/**
+ * Creates board
+ *
+ * @async
+ * @function postBoard
+ * @param {Board} board board
+ * @returns {Promise.<Board>} added board
+ */
 const postBoard = async (board) => {
   // TODO: mock implementation. should be replaced during task development
   BOARDS.push(board);
   return board;
 };
 
+/**
+ * Returns board by board ID
+ *
+ * @async
+ * @function getBoardById
+ * @param {number} boardId board ID
+ * @returns {Promise.<?Board>} board
+ */
+const getBoardById = async (id) =>
+  // TODO: mock implementation. should be replaced during task development
+  BOARDS.find((board) => board && board.id === id) || null;
+
+/**
+ * Updates board by board ID and returs updated board
+ *
+ * @async
+ * @function updateBoard
+ * @param {Board} newBoard updated board
+ * @returns {Promise.<?Board>} board
+ */
 const updateBoard = async (newBoard) => {
   const { id } = newBoard;
   // TODO: mock implementation. should be replaced during task development
@@ -33,6 +54,25 @@ const updateBoard = async (newBoard) => {
   if (boardIndex >= 0) {
     BOARDS[boardIndex] = newBoard;
     return BOARDS[boardIndex];
+  }
+  return null;
+};
+
+/**
+ * Deletes board by board ID
+ *
+ * @async
+ * @function deleteBoard
+ * @param {number} id board ID
+ * @returns {Promise.<boolean>} if board is deleted
+ */
+const deleteBoard = async (boardId) => {
+  // TODO: mock implementation. should be replaced during task development
+  const boardIndex = BOARDS.findIndex((board) => board && board.id === boardId);
+  if (boardIndex >= 0) {
+    BOARDS.splice(boardIndex, 1);
+    await deleteBoardInTasks(boardId);
+    return true;
   }
   return false;
 };

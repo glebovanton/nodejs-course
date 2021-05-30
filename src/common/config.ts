96ae18ca20b0866b3d30865ce6fbde388463ce5d
Anton <workglebov@gmail.com>
2021-05-30 23:47:09 +0300

    feat: Init TS

---
+++ src/common/config.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+interface IConfig {
+  PORT?: string;
+  NODE_ENV?: string;
+  MONGO_CONNECTION_STRING?: string;
+  JWT_SECRET_KEY?: string;
+  AUTH_MODE?: boolean;
+}
+
+dotenv.config({
+  path: path.join(__dirname, '../../.env'),
+});
+
+export const config: IConfig = {
+  PORT: process.env['PORT'],
+  NODE_ENV: process.env['NODE_ENV'],
+  MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
+  JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
+  AUTH_MODE: process.env['AUTH_MODE'] === 'true',
+};

ef2026aa453d150ba4621ebe9ff5d5a34ea3a72b
Anton <workglebov@gmail.com>
2021-06-06 15:17:26 +0300

    refactor: Replace runMorganLogging to errorHandler module

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { createWriteStream } from 'fs';
-import { INTERNAL_SERVER_ERROR } from 'http-status-codes';
-import morgan from 'morgan';
+import { INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status-codes';
 import * as path from 'path';
 import * as swaggerUI from 'swagger-ui-express';
 import * as YAML from 'yamljs';
@@ -9,25 +7,19 @@ import * as YAML from 'yamljs';
 import { boardRouter } from './resources/boards/board.router';
 import { userRouter } from './resources/users/user.router';
 import { taskRouter } from './resources/tasks/task.router';
-import { errorHandler } from './utils/errorHandler';
+import { errorHandler, runMorganLogging } from './utils/errorHandler';
 
 import { ErrorTypeEnum } from './types';
 
 const { exit } = process;
 const app = express();
-const accessErrSrteam = createWriteStream('log/accessErr.log');
+
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
 
-morgan.token('body', (req: Request): string => JSON.stringify(req.body));
-morgan.token('query', (req: Request): string => JSON.stringify(req.query));
-app.use(
-  morgan(
-    ':remote-addr - :remote-user [:date[clf]] ":referrer" ""Url- :url" "Method- :method" "Body-:body" "Query- :query" "Status- :status""',
-    { stream: accessErrSrteam }
-  )
-);
+runMorganLogging(app);
+
 process.on('unhandledRejection', (error: Error) => {
   errorHandler(error, ErrorTypeEnum.UnhandledRejection);
 });
@@ -35,7 +27,6 @@ process.on('uncaughtException', (error: Error) => {
   errorHandler(error, ErrorTypeEnum.UncaughtException);
   exit(1);
 });
-
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
@@ -49,6 +40,9 @@ app.use('/', (req: Request, res: Response, next: NextFunction) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
+app.get('*', (_req: Request, res: Response) => {
+  res.status(NOT_FOUND).send('Route is not found');
+});
 app.use((error: Error, _req: Request, res: Response) => {
   errorHandler(error, ErrorTypeEnum.UnhandledError);
   res
--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,7 +1,9 @@
-import { writeFileSync } from 'fs';
-
+import { Express, Request } from 'express';
+import { createWriteStream, writeFileSync } from 'fs';
+import morgan from 'morgan';
 import { ErrorTypeEnum } from '../types';
 
+const accessLogSrteam = createWriteStream('log/accessLog.log');
 const { stderr } = process;
 
 export const errorHandler = (error: Error, type: string): void => {
@@ -13,3 +15,15 @@ export const errorHandler = (error: Error, type: string): void => {
   );
   stderr.write(`${type} detected: ${isStack ? error.stack : error.message}`);
 };
+
+morgan.token('body', (req: Request): string => JSON.stringify(req.body));
+morgan.token('query', (req: Request): string => JSON.stringify(req.query));
+
+export function runMorganLogging(app: Express): void {
+  app.use(
+    morgan(
+      ':remote-addr - :remote-user [:date[clf]] ":referrer" ""Url- :url" "Method- :method" "Body-:body" "Query- :query" "Status- :status""',
+      { stream: accessLogSrteam }
+    )
+  );
+}

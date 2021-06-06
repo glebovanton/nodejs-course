95058447de8c4dd7ceef368d774e225d2a392f68
Anton <workglebov@gmail.com>
2021-06-06 15:52:09 +0300

    refactor: Replace error handlers to module

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-status-codes';
+import { NOT_FOUND } from 'http-status-codes';
 import * as path from 'path';
 import * as swaggerUI from 'swagger-ui-express';
 import * as YAML from 'yamljs';
@@ -7,11 +7,13 @@ import * as YAML from 'yamljs';
 import { boardRouter } from './resources/boards/board.router';
 import { userRouter } from './resources/users/user.router';
 import { taskRouter } from './resources/tasks/task.router';
-import { errorHandler, runMorganLogging } from './utils/errorHandler';
+import {
+  runInternalErrLogging,
+  runMorganLogging,
+  runUncaughtExceptLogging,
+  runUnhandledRejLogging,
+} from './utils/errorHandler';
 
-import { ErrorTypeEnum } from './types';
-
-const { exit } = process;
 const app = express();
 
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -20,13 +22,9 @@ app.use(express.json());
 
 runMorganLogging(app);
 
-process.on('unhandledRejection', (error: Error) => {
-  errorHandler(error, ErrorTypeEnum.UnhandledRejection);
-});
-process.on('uncaughtException', (error: Error) => {
-  errorHandler(error, ErrorTypeEnum.UncaughtException);
-  exit(1);
-});
+runUnhandledRejLogging();
+runUncaughtExceptLogging();
+
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
@@ -43,11 +41,6 @@ app.use('/boards', taskRouter);
 app.get('*', (_req: Request, res: Response) => {
   res.status(NOT_FOUND).send('Route is not found');
 });
-app.use((error: Error, _req: Request, res: Response) => {
-  errorHandler(error, ErrorTypeEnum.UnhandledError);
-  res
-    .status(INTERNAL_SERVER_ERROR)
-    .json({ success: false, message: error.message ? error.message : error });
-});
+runInternalErrLogging(app);
 
 export default app;
--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,12 +1,13 @@
-import { Express, Request } from 'express';
+import { Express, Request, Response } from 'express';
 import { createWriteStream, writeFileSync } from 'fs';
+import { INTERNAL_SERVER_ERROR } from 'http-status-codes';
 import morgan from 'morgan';
 import { ErrorTypeEnum } from '../types';
 
 const accessLogSrteam = createWriteStream('log/accessLog.log');
-const { stderr } = process;
+const { exit, stderr } = process;
 
-export const errorHandler = (error: Error, type: string): void => {
+function errorHandler(error: Error, type: string): void {
   const logFileName: string = type.split(' ').join('').toLowerCase();
   const isStack: boolean = type === ErrorTypeEnum.UnhandledError;
   writeFileSync(
@@ -14,7 +15,7 @@ export const errorHandler = (error: Error, type: string): void => {
     `${type} detected: ${error.message} \n`
   );
   stderr.write(`${type} detected: ${isStack ? error.stack : error.message}`);
-};
+}
 
 morgan.token('body', (req: Request): string => JSON.stringify(req.body));
 morgan.token('query', (req: Request): string => JSON.stringify(req.query));
@@ -26,4 +27,26 @@ export function runMorganLogging(app: Express): void {
       { stream: accessLogSrteam }
     )
   );
+}
+
+export function runInternalErrLogging(app: Express): void {
+  app.use((error: Error, _req: Request, res: Response) => {
+    errorHandler(error, ErrorTypeEnum.UnhandledError);
+    res
+      .status(INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: error.message ? error.message : error });
+  });
+}
+
+export function runUnhandledRejLogging(): void {
+  process.on('unhandledRejection', (error: Error) => {
+    errorHandler(error, ErrorTypeEnum.UnhandledRejection);
+  });
+}
+
+export function runUncaughtExceptLogging(): void {
+  process.on('uncaughtException', (error: Error) => {
+    errorHandler(error, ErrorTypeEnum.UncaughtException);
+    exit(1);
+  });
 }
